f0dfe51154f253d0b2888d2db6c99a8ca878b4fb
sangngoc27042001 <69387187+sangngoc27042001@users.noreply.github.com>
2022-04-21 12:38:28 +0700

    ae check mấy test đầu nha

--- initial/src/main/d96/checker/StaticCheck.py
+++ initial/src/main/d96/checker/StaticCheck.py
@@ -35,15 +35,73 @@ class StaticChecker(BaseVisitor,Utils):
         return self.visit(self.ast,StaticChecker.global_envi)
 
     def visitProgram(self,ast:Program, c):
+        programContex = {}
+        c.append(programContex)
         for x in ast.decl:
             self.visit(x, c)
-        return [self.visit(x,c) for x in ast.decl]
+        # return [self.visit(x,c) for x in ast.decl]
 
     def visitClassDecl(self,ast, c):
-        a = 'cal'
-        c.append(ast.classname.name)
-        # raise Redeclared(Attribute(),'a')
-    
+        programContex = c[-1]
+        if ast.classname.name in programContex.keys():
+            raise Redeclared(Class(),ast.classname.name)
+        else:
+            programContex[ast.classname.name] = {}
+            for x in ast.memlist:
+                self.visit(x, programContex[ast.classname.name])
+
+    def visitAttributeDecl(self,ast, c):
+        classContext = c
+        kind = ast.kind
+        if type(ast.decl) is VarDecl:
+            name = ast.decl.variable.name
+            typeat = ast.decl.varType
+            value = ast.decl.varInit
+        else:
+            name = ast.decl.constant.name
+            typeat = ast.decl.constType
+            value = ast.decl.value
+        if name in classContext.keys():
+            raise Redeclared(Attribute(), name)
+        else:
+            classContext[name] = [typeat, kind, name, value]
+
+    def visitMethodDecl(self,ast, c):
+        classContext = c
+        name = ast.name.name
+        if name in classContext.keys():
+            raise Redeclared(Method(), name)
+        kind = ast.kind
+        param = []
+        for x in ast.param:
+            param.append(self.visit(x,(param,'PARAMS')))
+        body = self.visit(ast.body,(c, param))
+        classContext[name] = [kind, name, param, body]
+
+    def visitVarDecl(self,ast, c):
+        if c[1] == 'PARAMS':
+            if ast.variable.name in [x[2] for x in c[0]]:
+                raise Redeclared(Parameter(), ast.variable.name)
+            return [ast.varType, Instance(), ast.variable.name, ast.varInit]
+        elif c[1] == 'INST':
+            if ast.variable.name in [x[2] for x in c[2]] + [x['information'][2] for x in c[3]]:
+                raise Redeclared(Variable(), ast.variable.name)
+            return {'instruction_ast': ast, 'information': [ast.varType, Instance(), ast.variable.name, ast.varInit]}
+
+    def visitConstDecl(self,ast, c):
+        if c[1] == 'INST':
+            if ast.constant.name in [x[2] for x in c[2]] + [x['information'][2] for x in c[3]]:
+                raise Redeclared(Constant(), ast.constant.name)
+            return {'instruction_ast': ast, 'information': [ast.constType, Instance(), ast.constant.name, ast.value]}
+
+    def visitBlock(self, ast, c):
+        param = c[1]
+        res = []
+        for inst in ast.inst:
+            if isinstance(inst,(VarDecl, ConstDecl)):
+                res += [self.visit(inst, (c,'INST',param, res))]
+        return res
+
 
     
 
--- initial/src/test/CheckerSuite.py
+++ initial/src/test/CheckerSuite.py
@@ -6,54 +6,78 @@ class CheckerSuite(unittest.TestCase):
     def test_400(self):
         """Simple program: int main() {} """
         input = """Class a{
-                    Var a:int;
-                    Var a:int;
+                    Var a:Int;
+                    Var a:Int;
                     }
                     Class b{}
                     Class c{}
                     Class d{}"""
         expect = "Redeclared Attribute: a"
         self.assertTrue(TestChecker.test(input,expect,400))
-#     def test_401(self):
-#         """Simple program: int main() {} """
-#         input = """Class a{}
-#         Class a{}"""
-#         expect = "Redeclared Class: a"
-#         self.assertTrue(TestChecker.test(input,expect,401))
-#     def test_402(self):
-#         """Simple program: int main() {} """
-#         input = """Class a{
-#                     a(a:int;a:tring){}
-#                     }"""
-#         expect = "Redeclared Parameter: a"
-#         self.assertTrue(TestChecker.test(input,expect,402))
-#     def test_403(self):
-#         """Simple program: int main() {} """
-#         input = """Class A{
-#     a(){}
-#     a(){}
-# }"""
-#         expect = "Redeclared Method: a"
-#         self.assertTrue(TestChecker.test(input,expect,403))
-#     def test_404(self):
-#         """Simple program: int main() {} """
-#         input = """Class a{
-#                     b(){
-#                         Var a:int;
-#                         Var a:int;
-#                     }
-#                     }"""
-#         expect = "Redeclared Variable: a"
-#         self.assertTrue(TestChecker.test(input,expect,404))
-#     def test_405(self):
-#         """Simple program: int main() {} """
-#         input = """Class a{
-#                     b(){
-#                         Val a:int = 1;
-#                         Val a:int = 1;
-#                     }
-#                     }"""
-#         expect = "Redeclared Constant: a"
-#         self.assertTrue(TestChecker.test(input,expect,405))
+    def test_401(self):
+        """Simple program: int main() {} """
+        input = """Class a{}
+        Class a{}"""
+        expect = "Redeclared Class: a"
+        self.assertTrue(TestChecker.test(input,expect,401))
+    def test_402(self):
+        """Simple program: int main() {} """
+        input = """Class a{
+                    a(a:int;a:tring){}
+                    }"""
+        expect = "Redeclared Parameter: a"
+        self.assertTrue(TestChecker.test(input,expect,402))
+
+    def test_403(self):
+        """Simple program: int main() {} """
+        input = """Class A{
+    Var b:Int;
+    a(){}
+    a(){}
+}"""
+        expect = "Redeclared Method: a"
+        self.assertTrue(TestChecker.test(input,expect,403))
+
+    def test_404(self):
+        """Simple program: int main() {} """
+        input = """Class a{
+                    b(){
+                        Var a:int;
+                        Var a:int;
+                    }
+                    }"""
+        expect = "Redeclared Variable: a"
+        self.assertTrue(TestChecker.test(input,expect,404))
+
+    def test_405(self):
+        """Simple program: int main() {} """
+        input = """Class a{
+                    b(){
+                        Val a:int = 1;
+                        Val a:int = 1;
+                    }
+                    }"""
+        expect = "Redeclared Constant: a"
+        self.assertTrue(TestChecker.test(input,expect,405))
+
+    def test_406(self):
+        """Simple program: int main() {} """
+        input = """Class a{
+                    b(a:int){
+                        Val a:int = 1;
+                    }
+                    }"""
+        expect = "Redeclared Constant: a"
+        self.assertTrue(TestChecker.test(input,expect,406))
+
+    def test_407(self):
+        """Simple program: int main() {} """
+        input = """Class a{
+                    b(a:int){
+                        Var a:int = 1;
+                    }
+                    }"""
+        expect = "Redeclared Variable: a"
+        self.assertTrue(TestChecker.test(input,expect,407))
 
     
