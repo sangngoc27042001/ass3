679a5419c091a2250b9b36447ba1cf1a9c9477f7
sangngoc27042001 <69387187+sangngoc27042001@users.noreply.github.com>
2022-05-06 13:31:10 +0700

    a

--- initial/src/generateSubmission.py
+++ initial/src/generateSubmission.py
@@ -2,12 +2,12 @@ import os
 import shutil
 os.system('python run.py gen')
 try:
-    os.mkdir("../submission")
+    os.mkdir("../../../submissionass3")
 except:
     print("The folder submission already created")
 
 CheckSuite = 'test/CheckerSuite.py'
 StaticCheck='main/d96/checker/StaticCheck.py'
 
-shutil.copy(CheckSuite,'../submission/CheckerSuite.py')
-shutil.copy(StaticCheck,'../submission/StaticCheck.py')
+shutil.copy(CheckSuite,'../../../submissionass3/CheckerSuite.py')
+shutil.copy(StaticCheck,'../../../submissionass3/StaticCheck.py')
